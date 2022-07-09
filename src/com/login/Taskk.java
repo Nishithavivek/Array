de758f7ae8a3f3b0785d6aa09cf8aa00335a5885
Nishithavivek <viveknishitha98@gmail.com>
2022-07-09 15:15:35 +0530

    Message

---
+++ src/com/login/Taskk.java
@@ -0,0 +1,28 @@
+package com.login;
+
+public class Taskk {
+
+	public static void main(String[] args) {
+	
+     int a [][]=new int [2][2];
+     a[0][0]=10;
+     a[0][1]=20;
+     a[1][0]=30;
+     a[1][1]=40;
+     
+     System.out.println("===for loop===");
+     
+     for(int i=0; i<a.length; i++) {
+    	 for(int j=0; j<a.length; j++) {
+    		 System.out.print(a[i][j]+ "  ");
+    	 }
+    	 System.out.println();
+     }
+    	 System.out.println("===enhance for loop===");
+     
+     for (int[] b:a) {
+    	 for(int c:b)
+    	 System.out.println(c);
+     }
+	}
+}
